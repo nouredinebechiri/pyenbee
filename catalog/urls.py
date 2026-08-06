4e1e34245128e492293d18877b1271f7a100ea24
Nouredine Bechiri <nouredine.bechiri@gmail.com>
2026-08-06 11:58:43 +0100

    06082026

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='catalog')
+    path('home', views.index, name='index'),
+    path('book-create', views.BookCreate.as_view(), name='book_create'),
+    path('book-list', views.BookList.as_view(), name='book_list'),
+    path('book-detail/<int:pk>', views.BookDetail.as_view(), name='book_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from . import models
+from django.views import generic
 
 
 def index(request):
-    return HttpResponse(('<h1>Catalog page</h1>'))
+    num_books = models.Book.objects.all().count()
+    num_book_intance = models.BookInstance.objects.all().count()
+    num_book_available = models.BookInstance.objects.filter(status__exact='a').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_book_intance': num_book_intance,
+        'num_book_available': num_book_available
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+class BookCreate(generic.CreateView):
+    model = models.Book
+    fields = '__all__'
+    template_name = 'catalog/book_create.html'
+    success_url = 'home'
+
+
+class BookList(generic.ListView):
+    model = models.Book
+    template_name = 'catalog/book_list.html'
+    context_object_name = 'books'
+    
+
+class BookDetail(generic.DeleteView):
+    model = models.Book
+    template_name = 'catalog/book_detail.html'
+    context_object_name = 'book'
+
+    
 
 
